6e7ecf458d7bc675c6cb9f65c94f420aa07c7526
Charlie Hsieh <charlieofficial@gmail.com>
2020-02-03 15:31:32 +0800

    finished testing aasm

--- README.md
+++ README.md
@@ -15,6 +15,16 @@ Things you may want to cover:
   * gem 'pry-rails', :group => :development
   * gem 'bullet', group: 'development'
 
+* Other features built-in is used
+  * SJR
+  * yarn add jquery
+  * test_rails_aasm/app/javascript/packs/application.js
+
+    ```bash
+    import "jquery/src/jquery"
+    ...
+    ```
+
 * Changes
   * https://github.com/charlietag/test_rails_aasm/compare/v0.0.0...master
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,26 @@
 class Book < ApplicationRecord
+  include AASM
+
+  aasm do
+
+    state :writing, initial: true
+    state :reviewing, :printing, :published
+
+    event :write do
+      transitions from: [:reviewing, :printing, :published], to: :writing
+    end
+
+    event :review do
+      transitions from: :writing, to: :reviewing
+    end
+
+    event :print do
+      transitions from: :reviewing, to: :printing
+    end
+
+    event :publish do
+      transitions from: :printing, to: :published
+    end
+
+  end
 end
